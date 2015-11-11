3da9c6eb4ab1272e14b599798b96174f6da213e4
Ross Zurowski <ross@rosszurowski.com>
2015-11-11 16:53:38 -0500

    Move to canvas

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ import * as number from './lib/number'
 var body = d3.select('body')
 var padding = 40
 
+var TWO_PI = Math.PI * 2
+
 function init (data) {
 
 	var days = d3.nest()
 		.key(d => date.format.yearMonthDay(d.last_visit_time))
 		.entries(data)
 
-	var svg = body.append('svg')
+	var canvas = body.append('canvas')
 		.classed('canvas', true)
+	canvas
+		.attr('width', canvas.node().clientWidth)
+		.attr('height', canvas.node().clientHeight)
+	var ctx = canvas.node().getContext('2d')
+	ctx.imageSmoothingEnabled = false
 
 	body.select('[data-bind="pages"]').text(number.withThousandsComma(data.length))
 	body.select('[data-bind="days"]').text(days.length)
@@ -35,29 +42,45 @@ function init (data) {
 	// x is columns by day
 	var x = d3.scale.linear()
 		.domain([0, d3.keys(days).length])
-		.range([padding, svg.node().clientWidth - padding])
+		.range([padding, canvas.node().clientWidth - padding])
 	// y is by time of day
 	var y = d3.time.scale()
-		.range([padding, svg.node().clientHeight - padding])
+		.range([padding, canvas.node().clientHeight - padding])
 
-	var columns = svg.selectAll('g')
-			.data(days)
-		.enter().append('g')
-			.attr('class', 'column')
-			.attr('transform', (d, i) => `translate(${x(i)}, 0)`)
+	render()
+	// d3.timer(render)
+	d3.select(window).on('resize', _.throttle(resize, 200))
 
-	columns.selectAll('circle')
-			.data(day => day.values)
-		.enter().append('circle')
-			.attr('class', 'node')
-			.attr('cy', d => {
-				y.domain(date.extentOfDay(d.last_visit_time))
-				return y(d.last_visit_time)
+	function render () {
+		clear(ctx, canvas.node())
+		ctx.fillStyle = 'rgba(246, 138, 103, 0.05)'
+		days.forEach((day, i) => {
+			var left = x(i)
+			y.domain(date.extentOfDay(day.values[0].last_visit_time))
+			day.values.forEach((d, i) => {
+				var top = y(d.last_visit_time)
+				ctx.beginPath()
+				ctx.arc(Math.round(left), top, 2, 0, TWO_PI, true)
+				ctx.closePath()
+				ctx.fill()
 			})
+		})
+	}
 
+	function clear (ctx, canvas) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+	}
 
+	function resize () {
+		canvas
+			.attr('width', canvas.node().clientWidth)
+			.attr('height', canvas.node().clientHeight)
+		x.range([padding, canvas.node().clientWidth - padding])
+		y.range([padding, canvas.node().clientHeight - padding])
+		render()
+	}
 
-	setTimeout(() => body.classed('loading', false), 500)
+	setTimeout(() => body.classed('loading', false), 25)
 }
 
 /**
