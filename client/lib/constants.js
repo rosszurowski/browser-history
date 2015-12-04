2443af2bce1e33b7336b8cbcf3a1204dab55b6af
Ross Zurowski <ross@rosszurowski.com>
2015-12-04 10:40:24 -0500

    Add brand colors

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -18,12 +18,20 @@ export const FAVORITE_COUNT = 5
  */
 export const PADDING = 40
 
-export const DEFAULT_FILL_STYLE = 'rgba(246, 138, 103, 0.05)'
+export const DEFAULT_FILL_STYLE = 'rgba(127, 127, 127, 0.05)'
 export const DOMAIN_FILL_STYLES = {
+	// Local
 	'localhost:8080': 'rgba(232, 241, 134, 0.2)',
-	'docs.google.com': 'rgba(232, 241, 134, 0.2)',
-	'facebook.com': 'rgba(134, 151, 241, 0.2)',
+	// Google Services
+	'google.com': 'rgba(245, 249, 255, 0.2)',
+	'docs.google.com': 'rgba(245, 249, 255, 0.2)',
+	'mail.google.com': 'rgba(245, 249, 255, 0.2)',
+	// Popular Sites
 	'youtube.com': 'rgba(247, 59, 59, 0.2)',
-	'mail.google.com': 'rgba(232, 241, 134, 0.2)',
-	'reddit.com': 'rgba(232, 241, 134, 0.2)'
+	'facebook.com': 'rgba(59, 89, 152, 0.2)',
+	'instagram.com': 'rgba(63, 114, 156, 0.2)',
+	'reddit.com': 'rgba(255, 69, 0, 0.2)',
+	// Developer Sites
+	'news.ycombinator.com': 'rgba(255, 102, 0, 0.2)',
+	'stackoverflow.com': 'rgba(242, 131, 0, 0.2)',
 }
